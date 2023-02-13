e739b55a1c7709125c774a939001031779628e79
musaevabdulaziz <amusaev@exadel.com>
2023-02-13 16:14:44 +0500

    Combined two default routers into one

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^auth/', include('djoser.urls')),
-    re_path(r"^auth/", include('djoser.urls.jwt')),
-    path('', include('restaurant.urls')),
+    path('api/', include('restaurant.urls')),
 ]
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include, re_path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import MenuItemViewSet
 from djoser import views as djoser_views 
@@ -6,11 +6,9 @@ from djoser import views as djoser_views
 
 router = DefaultRouter()
 router.register(r'menu-items', MenuItemViewSet, basename="menu-item"),
-# router.register(r'users', djoser_views.UserViewSet),
-# router.register(r'jwt', djoser_jwt.)
+router.register(r'users', djoser_views.UserViewSet),
 
 urlpatterns = [
     path('', include(router.urls)),
-    # re_path(r'^api/', include('djoser.urls')),
-    # re_path("", include('djoser.urls.jwt')),
+    path("", include('djoser.urls.jwt')),
 ]
